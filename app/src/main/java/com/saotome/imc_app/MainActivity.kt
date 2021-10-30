eb5cd25eddb81e8f349a4ebf434d46e8ec700e3d
Saotomefirst <jorge.appleseed@gmail.com>
2021-10-30 16:57:32 -0300

    Explorando o Ciclo de Vida
    Criados exemplos dos métodos para o ciclo de vida de um app, desde seu nascimento até sua morte - acompanhe pelo LogCat com a tag [ lifecycle ]

--- app/src/main/java/com/saotome/imc_app/MainActivity.kt
+++ app/src/main/java/com/saotome/imc_app/MainActivity.kt
@@ -2,10 +2,47 @@ package com.saotome.imc_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.w("lifecycle","Entrei no CREATE - criando a tela")
+        // atalho do onCreate direto para o onDestoy
+        //finish()
     }
+
+    override fun onStart() {
+        super.onStart()
+        Log.w("lifecycle","Entrei no START - tornando a tela visível")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.w("lifecycle","Entrei no RESUME - tornando a tela interativa")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.w("lifecycle","PAUSE - a tela perdeu o foco e a interatividade")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.w("lifecycle","STOP - a tela não está mais visível, mas ainda existe")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.w("lifecycle","\nEntrei no RESTART - a tela invisível está retornando ao foco")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.w("lifecycle","Entrei no DESTROY - a tela foi destruida")
+    }
+
+
 }
