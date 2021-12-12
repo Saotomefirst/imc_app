226ac0755f9c3d024ea04bcfcc090e0c94392781
Saotomefirst <jorge.appleseed@gmail.com>
2021-12-12 18:11:06 -0300

    Convertendo Variáveis do Synthetic para o View Binding
    O pacote Synthetic foi deprecado! Usa-se agora o Binding
        https://developer.android.com/topic/libraries/view-binding/migration#kts
    
    Todas as variáveis do código foram atualizadas para o novo padrão

--- app/src/main/java/com/saotome/imc_app/MainActivity.kt
+++ app/src/main/java/com/saotome/imc_app/MainActivity.kt
@@ -4,16 +4,62 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.PersistableBundle
 import android.util.Log
+import android.widget.Toast
+import androidx.core.widget.doAfterTextChanged
+import androidx.core.widget.doOnTextChanged
+import com.saotome.imc_app.databinding.ActivityMainBinding
+import java.util.*
+
+//import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
+
+    /*
+    O pacote Synthetic foi deprecado! Usa-se agora o Binding
+    https://developer.android.com/topic/libraries/view-binding/migration#kts
+     */
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        Log.w("lifecycle","Entrei no CREATE - criando a tela")
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        //setContentView(R.layout.activity_main)
+
+        //Log.w("lifecycle","Entrei no CREATE - criando a tela")
         // atalho do onCreate direto para o onDestoy
         //finish()
+        setListeners()
     }
 
+    private fun setListeners () {
+        binding.alturaEDT?.doAfterTextChanged { text ->
+            Toast.makeText(this, "${text.toString()}", Toast.LENGTH_SHORT).show()
+        }
+        binding.pesoEDT?.doOnTextChanged { text, start, before, count ->  
+            binding.titleTXT?.text = text
+        }
+
+        binding.calcularBTN?.setOnClickListener {
+            CalcularIMC(altura = binding.alturaEDT?.text.toString(), peso = binding.pesoEDT?.text.toString())
+        }
+    }
+
+    private fun CalcularIMC (peso: String, altura: String) {
+        val peso = peso.toFloatOrNull()
+        var altura = altura.toFloatOrNull()
+
+        if (peso != null && altura != null ) {
+            altura /= 100
+            val imc = peso / (altura * altura)
+            val local = Locale("pt", "BR")
+            binding.titleTXT.text = "Meu IMC é: %.2f".format(local, imc)
+        }
+    }
+
+    /*
     override fun onStart() {
         super.onStart()
         Log.w("lifecycle","Entrei no START - tornando a tela visível")
@@ -43,6 +89,6 @@ class MainActivity : AppCompatActivity() {
         super.onDestroy()
         Log.w("lifecycle","Entrei no DESTROY - a tela foi destruida")
     }
-
+    */
 
 }
